0312144687380e7f37f583e49825c2e249811f14
Zackky Muhammad <m.zackky@gmail.com>
2021-06-21 21:53:07 +0700

    fix(completion): fix completion long desc

--- cmd/completion.go
+++ cmd/completion.go
@@ -11,32 +11,48 @@ To load completions:
 
 Bash:
 
-$ source <(go-project completion bash)
+    $ source <(go-project completion bash)
 
-# To load completions for each session, execute once:
-Linux:
-  $ go-project completion bash > /etc/bash_completion.d/go-project
-MacOS:
-  $ go-project completion bash > /usr/local/etc/bash_completion.d/go-project
+    To load completions for each session, execute once:
+
+    Linux:
+
+      $ go-project completion bash > /etc/bash_completion.d/go-project
+
+    MacOS:
+
+      $ go-project completion bash > /usr/local/etc/bash_completion.d/go-project
 
 Zsh:
 
-# If shell completion is not already enabled in your environment you will need
-# to enable it.  You can execute the following once:
+    If shell completion is not already enabled in your environment you will need
+    to enable it.  You can execute the following once:
 
-$ echo "autoload -U compinit; compinit" >> ~/.zshrc
+      $ echo "autoload -U compinit; compinit" >> ~/.zshrc
 
-# To load completions for each session, execute once:
-$ go-project completion zsh > "${fpath[1]}/_go-project"
+    To load completions for each session, execute once:
 
-# You will need to start a new shell for this setup to take effect.
+      $ go-project completion zsh > "${fpath[1]}/_go-project"
+
+    You will need to start a new shell for this setup to take effect.
 
 Fish:
 
-$ go-project completion fish | source
+    $ go-project completion fish | source
 
-# To load completions for each session, execute once:
-$ go-project completion fish > ~/.config/fish/completions/go-project.fish
+    To load completions for each session, execute once:
+
+      $ go-project completion fish > ~/.config/fish/completions/go-project.fish
+
+PowerShell:
+
+    PS> go-project completion powershell | Out-String | Invoke-Expression
+
+    To load completions for every new session, run:
+
+      PS> go-project completion powershell > go-project.ps1
+
+    And source this file from your PowerShell profile.
 `
 
 var completionCmd = &cobra.Command{
